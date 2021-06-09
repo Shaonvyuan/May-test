2ab3cfe22e3294e9be9b14839adc06efdf413f59
Shaonvyuan <melodyer@163.com>
2021-06-09 21:10:55 +0800

    revise i to _

--- may-test/test5_a_aa_aaa.py
+++ may-test/test5_a_aa_aaa.py
@@ -4,7 +4,7 @@ n = int(input("Input number:"))
 a = int(input("Input a:"))
 temp = 0
 s = 0
-for i in range(1, n + 1):
+for _ in range(1, n + 1):
     temp = temp * 10 + a
     s += temp
 print(f"s={s}")
